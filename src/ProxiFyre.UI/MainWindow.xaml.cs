d415d4375ba52e73c0a915213f82b4126cd8389a
ghost <alializadehwb@gmail.com>
2026-07-05 01:01:45 +0400

    feat(ui): scaffold unpackaged WinUI 3 app + CI build job

---
+++ src/ProxiFyre.UI/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace ProxiFyre.UI;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Title = "ProxiFyre";
+    }
+}
